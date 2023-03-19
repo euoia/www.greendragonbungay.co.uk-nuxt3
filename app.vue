acb8726a7b61ab0f5bdeaeab31b752af9784b1da
James Pickard <james.pickard@gmail.com>
2023-03-19 13:28:11 +0000

    Initial commit

---
+++ app.vue
@@ -0,0 +1,11 @@
+<template>
+  <gd-page>
+  <section>
+    <h1>About the Green Dragon</h1>
+    <p>
+      The Green Dragon is a pub in Bree, located in the Shire. It is the
+      only inn in the village, and is run by the innkeeper, Drogo Baggins.
+    </p>
+  </section>
+  </gd-page>
+</template>
---
+++ components/gd-page.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="gd-page"><slot /></div>
+</template>
+
+<script lang="scss">
+.gd-page {
+  display: flex;
+  gap: 1em;
+  margin: auto;
+  max-width: 800px;
+}
+</script>
