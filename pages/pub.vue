7f78790438e0b60024387274c9d17fa0b44c08d6
James Pickard <james.pickard@gmail.com>
2023-05-11 10:46:55 +0100

    Add beers

--- pages/pub.vue
+++ pages/pub.vue
@@ -9,8 +9,43 @@
       alt: 'Green Dragon, Bungay, Suffolk, logo',
     }"
   >
+    <gd-section title="Our Beer">
+      <gd-p
+        ><b>Chaucer Ale 3.7% <small>vol</small> &horbar; Challenger hops</b
+        ><br />
+        A cask-conditioned full-flavoured hoppy session bitter. The local's
+        favourite.</gd-p
+      >
+
+      <gd-p
+        ><b
+          >Bridge Street Bitter 4.5% <small>vol</small> &horbar; Challenger
+          hops</b
+        ><br />
+        A cask-conditioned strong hoppy bitter with good malt character and
+        slightly spicy notes.</gd-p
+      >
+
+      <gd-p
+        ><b>Dragon Gold 4.4% <small>vol</small> &horbar; Bobek hops</b><br />
+        A cask-conditioned light golden ale with lemon and pine notes.</gd-p
+      >
+
+      <gd-p
+        ><b>Strong Mild 5.5% <small>vol</small> &horbar; Bobek hops</b><br />
+        A cask-conditioned mild with rich flavours &horbar; stronger than your
+        usual mild!</gd-p
+      >
+
+      <gd-p
+        ><b>SIPA 4.5% <small>vol</small> &horbar; Cascade hops</b><br />
+        A cask-conditioned session IPA &horbar; bitter and aromatic with a
+        lasting finish.</gd-p
+      >
+    </gd-section>
+
     <gd-section
-      title="The Green Dragon"
+      title="Our Pub"
       images-orientation="landscape"
       :images="[
         {
@@ -35,7 +70,7 @@
     </gd-section>
 
     <gd-section
-      title="Our brewery"
+      title="Our Brewery"
       :images="[
         {
           src: '/images/beer-hops-and-malt.jpg',
